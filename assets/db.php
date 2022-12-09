b95936c2cecbb6076d24b33dc6749d2bc45f7f31
Moontasir Abtahee <mamoontasir.abtahee@gmail.com>
2022-12-09 23:03:13 +0600

    Login and User Done except fund transfer

---
+++ assets/db.php
@@ -0,0 +1,34 @@
+<?php 
+    $con = new mysqli('localhost','root','','bms');
+    define('bankname', 'Federal Bank of BRACU',true);
+    $uid = '';
+    if(isset($_SESSION['userId'])){
+    $uid = $_SESSION['userId'];
+    $flag = "user";}
+    if(isset($_SESSION['cashId'])){
+    $uid = $_SESSION['cashId'];
+    $flag = "cash";}
+    if(isset($_SESSION['managerId'])){
+    $uid = $_SESSION['managerId'];
+    $flag = "manager";}
+    $userData = [];
+    if(!empty($uid) and $flag == "user"){
+      $ar = $con->query("select * from customer cs,account ac,branch br where cs.userID = ac.userID and cs.branchId = br.branchId");
+      $userData = $ar->fetch_assoc();
+    }
+
+    if(!empty($uid) and $flag == "cash"){
+      $ar = $con->query("Select * from employee em , branch br where em.branchId = br.branchId ");
+      $userData = $ar->fetch_assoc();
+    }
+
+    if(!empty($uid) and $flag == "manager"){
+      $ar = $con->query("Select * from employee em , branch br where em.branchId = br.branchId ");
+      $userData = $ar->fetch_assoc();
+    }
+?>
+<script type="text/javascript">
+	$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+</script>
